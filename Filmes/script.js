7e4b2a2cff79f7de0d63b6025c8b82bed4cfa5b5
Paulo <Paulorcg063@gmail.com>
2025-11-23 15:23:43 -0300

    Alteração na responsividade do primeiro filme

--- Filmes/script.js
+++ Filmes/script.js
@@ -1,45 +1,45 @@
-const slider = document.querySelector("#filmes-series");
-const slides = document.querySelectorAll(".img-filmes");
+const container = document.querySelector("#filmes-series");
+const wrapper = document.querySelector(".filmes-wrapper");
 const btnNext = document.getElementById("next");
 const btnPrev = document.getElementById("prev");
-let index = 0;
 
-// Função para atualizar o slider e controlar a visibilidade dos botões
-function updateSlider() {
-    slider.style.transform = `translateX(-${index * 100}%)`;
+let currentTranslate = 0;
+let slide = 400;
 
-    // Oculta o botão "prev" no primeiro slide
-    btnPrev.style.display = index === 0 ? "none" : "block";
-
+function updateLimits() {
+    containerWidth = container.offsetWidth;
+    wrapperWidth = wrapper.scrollWidth;
+    maxTranslate = containerWidth - wrapperWidth; // sempre negativo
 }
 
-btnNext.addEventListener("click", () => {
-    btnNext.style.display = "none"; // Esconde o botão
-  });
+updateLimits(); // calcula ao iniciar
+window.addEventListener("resize", updateLimits); // recalcula se a tela mudar
 
-btnPrev.addEventListener("click", () => {
-    btnNext.style.display = "block"; // Mostra o botão direito
-});
-
-// Evento para avançar
 btnNext.addEventListener("click", () => {
-    if (index < slides.length - 1) {
-        index++;
-        updateSlider();
+    updateLimits();
+
+    if (currentTranslate > maxTranslate) {
+        currentTranslate -= slide;
+
+        // limite máximo
+        if (currentTranslate < maxTranslate) {
+            currentTranslate = maxTranslate;
+        }
+
+        wrapper.style.transform = `translateX(${currentTranslate}px)`;
     }
 });
 
-// Evento para voltar
 btnPrev.addEventListener("click", () => {
-    if (index > 0) {
-        index--;
-        updateSlider();
+    updateLimits();
+
+    if (currentTranslate < 0) {
+        currentTranslate += slide;
+
+        // limite mínimo (começo)
+        if (currentTranslate > 0) {
+            currentTranslate = 0;
+        }
+        wrapper.style.transform = `translateX(${currentTranslate}px)`;
     }
 });
-
-// Inicializa o estado dos botões
-updateSlider();
-
-
-
-    
